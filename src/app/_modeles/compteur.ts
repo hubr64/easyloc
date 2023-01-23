84d6bdb4ec37f6471ca1fb462b8a4d002bfc6d21
hubr64 <hubr.md@gmail.com>
2023-01-23 11:46:34 +0100

    Création du modèle de compteurs et intégration au modèle complet

---
+++ src/app/_modeles/compteur.ts
@@ -0,0 +1,110 @@
+import { Piece } from './piece';
+import { Bien } from './bien';
+
+export class CompteurValue {
+
+    className: string;
+    dateReleve: Date;
+    valeur: any;
+    preuve: Piece | null;
+  
+    constructor() {
+      this.className = 'CompteurValue';
+      this.dateReleve = new Date();
+      this.preuve = null;
+    }
+
+    static fromJSON(input: any, docPieces: Piece[] = []): CompteurValue {
+        var tmp: CompteurValue = Object.assign(new CompteurValue(), input);
+        //Build correct dates
+        tmp.dateReleve = new Date(tmp.dateReleve);
+
+        //Convert piece id to references to pieces
+        if(input.preuve){ 
+            docPieces.forEach((docPiece:Piece) => {
+                if(docPiece.id == input.preuve){
+                    tmp.preuve = docPiece;
+                }
+            });
+        }
+        return tmp;
+    }
+
+    toJSON(): any {
+        let serialize = {
+            dateReleve: this.dateReleve,
+            valeur: this.valeur,
+            preuve: this.preuve ? this.preuve.id : '',
+        };
+        return serialize;
+    }
+  
+    toString(): string{
+      return this.valeur + "(" + this.dateReleve.toLocaleDateString() + ")";
+    }
+  
+  }
+
+export class Compteur {
+    className: string;
+    id: string;
+    designation: string;
+    bien: Bien;
+    valeurs : CompteurValue[];
+
+    constructor() {
+        this.className = 'Compteur';
+        this.id = '';
+        this.designation = '';
+        this.valeurs = [];
+    }
+
+    static fromJSON(input: any, docPieces: Piece[] = [], docBiens: Bien[] = []): Compteur {
+        var tmp: Compteur = Object.assign(new Compteur(), input);
+
+        //Convert bien id to reference to bien
+        if(input.bien){ 
+            docBiens.forEach((docBien:Bien) => {
+                if(docBien.id == input.bien){
+                    tmp.bien = docBien;
+                }
+            });
+        }
+        //Load valeurs
+        if(input.valeurs){
+            tmp.valeurs = [];
+            for (const key of Object.keys(input.valeurs)) {
+                tmp.valeurs.push(CompteurValue.fromJSON(input.valeurs[key], docPieces));
+            }
+        }
+        return tmp;
+    }
+
+    toJSON(): any {
+        let serialize = {
+            id: this.id,
+            designation: this.designation,
+            bien: this.bien ? this.bien.id: '',
+            valeurs: [''],
+        };
+        this.valeurs.forEach((valeur: CompteurValue) => {
+            serialize.valeurs.push(valeur.toJSON());
+        });
+        return serialize;
+    }
+
+    toString(): string{
+        return this.designation + "(" + this.id + ")";
+    }
+
+    public get derniereValeur(): CompteurValue|null {
+        var last: CompteurValue|null = null;
+        this.valeurs.forEach((valeur: CompteurValue) => {
+            if(!last || (last && valeur.dateReleve > last.dateReleve) ){
+                last = valeur;
+            }
+        });
+        return last?last: null;
+    }
+
+}
